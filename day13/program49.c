600b300e25b712b14b2510db69470dc0a28f766f
Apoorvcode28 <apoorvchaturvedi23@gmail.com>
2026-06-13 10:53:09 +0530

    Add files via upload

---
+++ day13/program49.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    int n, i;
+
+    printf("Enter the size of the array: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    printf("Enter %d elements:\n", n);
+    for(i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Array elements are: ");
+    for(i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
