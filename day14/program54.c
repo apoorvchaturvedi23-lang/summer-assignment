ef35fd6ecf073dfd53ca11c64ead3b6739762ec5
Apoorvcode28 <apoorvchaturvedi23@gmail.com>
2026-06-14 23:43:41 +0530

    Add files via upload

---
+++ day14/program54.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+    int n, i, key, count = 0;
+
+    printf("Enter the size of the array: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    printf("Enter %d elements:\n", n);
+    for(i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Enter the element to find frequency: ");
+    scanf("%d", &key);
+
+    for(i = 0; i < n; i++) {
+        if(arr[i] == key) {
+            count++;
+        }
+    }
+
+    printf("Frequency of %d = %d\n", key, count);
+
+    return 0;
+}
